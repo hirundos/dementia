38a23654fb8c80f43487d0eee1b7b3332d5d8c55
hirundos <dkdus591@gmail.com>
2020-12-05 17:52:39 +0900

    add game

--- app/src/main/java/com/dkdus/dementia/ui/game/GameFragment.kt
+++ app/src/main/java/com/dkdus/dementia/ui/game/GameFragment.kt
@@ -1,24 +1,37 @@
 package com.dkdus.dementia.ui.game
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.dkdus.dementia.R
+import kotlinx.android.synthetic.main.fragment_game.*
 
-class GameFragment : Fragment() {
+class GameFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        val root = inflater.inflate(R.layout.fragment_game, container, false)
         return root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.gugu_btn) {
+            var intent : Intent = Intent(context, GuguActivity::class.java)
+            startActivity(intent)
+        }else if(p0?.id == R.id.rcp_btn){
+            var intent : Intent = Intent(context, RcpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/dkdus/dementia/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dkdus/dementia/ui/home/HomeFragment.kt
@@ -1,21 +1,33 @@
 package com.dkdus.dementia.ui.home
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.dkdus.dementia.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
+    val CITY_HALL = LatLng(37.5662952, 126.97794509999994)
+    var googleMap : GoogleMap? = null
     lateinit var viewModel: HomeViewModel
     val contextHome by lazy { this }
     val PERMISSIONS = arrayOf(
@@ -23,6 +35,20 @@ class HomeFragment : Fragment() {
         Manifest.permission.ACCESS_FINE_LOCATION)
 
     val REQUEST_PERMISSION_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+  //      mapView.onCreate(savedInstanceState)
+
+        if(checkPermissions()){
+//            initMap()
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), PERMISSIONS, REQUEST_PERMISSION_CODE)
+        }
+   //     myLocationButton.setOnClickListener {  }
+
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -37,30 +63,94 @@ class HomeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        placeCall()
+   //     placeCall()
     }
 
     private fun placeCall(){
         viewModel.callPlace()
     }
 
-    fun checkPermissions() : Boolean {
+    private fun checkPermissions() : Boolean {
         for(permission in PERMISSIONS){
-            /*
-            if(ActivityCompat.checkSelfPermission(contextHome, permission) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.checkSelfPermission(requireActivity(), permission) != PackageManager.PERMISSION_GRANTED){
                 return false
             }
-            
-             */
         }
         return true
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        initMap()
     }
+
+    @SuppressLint("MissingPermission")
+    private fun initMap() {
+        mapView.getMapAsync {
+            googleMap = it
+            it.uiSettings.isMyLocationButtonEnabled = false
+
+            when{
+                checkPermissions() -> {
+                    if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION )
+                        != PackageManager.PERMISSION_GRANTED) {
+                        return@getMapAsync
+                    }
+                    it.isMyLocationEnabled = true
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(CITY_HALL, 17f))
+                }
+                else->{
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(CITY_HALL, 17f))
+                }
+            }
+
+        }
+    }
+
+    /*
+    @SuppressLint("MissingPermission")
+    fun getMyLocation() : LatLng{
+        val locationProvider: String = LocationManager.GPS_PROVIDER
+
+
+        val locationManager =  getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val lastKnownLocation: Location? = locationManager.getLastKnownLocation(locationProvider)
+        return LatLng(lastKnownLocation?.latitude!!, lastKnownLocation.longitude)
+    }
+
+
+
+    private fun onMyLocationButtonClick() {
+        when {
+            checkPermissions() -> googleMap?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(getMyLocation(), 17f)
+            )
+            else -> Toast.makeText(requireContext(), "위치사용권한 설정에 동의해주세요", Toast.LENGTH_LONG).show()
+        }
+    }
+
+     */
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
 }
