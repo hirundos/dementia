cc756afab1b52540ae05bc415b1ab3dc10f0b88a
hirundos <dkdus591@gmail.com>
2020-12-08 15:19:14 +0900

    자세히 보기 기능

--- app/src/main/java/com/dkdus/dementia/ui/DetailActivity.kt
+++ app/src/main/java/com/dkdus/dementia/ui/DetailActivity.kt
@@ -3,10 +3,31 @@ package com.dkdus.dementia.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dkdus.dementia.R
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+
+        if(intent!=null){
+            var name = intent.getStringExtra("cntername")
+            intent.getStringExtra("lat")
+            intent.getStringExtra("lot")
+            var addr = intent.getStringExtra("lnmadr")//주소
+            var programShow = intent.getStringExtra("imbcltyIntrcn")//주요치매관리프로그램소개
+            var phoneNum = intent.getStringExtra("call")
+            var docN = intent.getStringExtra("doc")
+            var nurN = intent.getStringExtra("nur")
+
+            tv_name.text = name
+            tv_addrV.text = addr
+            tv_programValue.text = programShow
+            tv_callV.text = phoneNum
+            tv_docN.text = docN
+            tv_nurseN.text = nurN
+        }
+
     }
 }
--- app/src/main/java/com/dkdus/dementia/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dkdus/dementia/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.dkdus.dementia.ui.home
 
 import android.Manifest
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,11 +12,14 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.dkdus.dementia.R
 import com.dkdus.dementia.model.Item
+import com.dkdus.dementia.ui.DetailActivity
+import com.dkdus.dementia.ui.game.RcpActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.w3c.dom.Element
 import org.w3c.dom.Node
@@ -25,10 +29,18 @@ import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
 
 
-class HomeFragment : Fragment(), OnMapReadyCallback {
+class HomeFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     lateinit var viewModel: HomeViewModel
     private lateinit var mMap: GoogleMap
+    var lat : NodeList? = null
+    var lot : NodeList? = null
+    var cntername : NodeList? = null
+    var lnmadr : NodeList? = null
+    var imbcltyIntrcn : NodeList? = null
+    var call : NodeList? = null
+    var doctorN : NodeList? = null
+    var nurseN : NodeList? = null
     var data: MutableList<Item> = arrayListOf()
 
 
@@ -43,11 +55,6 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         return root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -59,32 +66,45 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         viewModel.callPlace().observe(viewLifecycleOwner, Observer {
             var xpath: XPath = XPathFactory.newInstance().newXPath()
 
-            var lat = xpath.evaluate(
+            lat = xpath.evaluate(
                 "//body/items/item/latitude",
                 it, XPathConstants.NODESET
             ) as NodeList?
-            var lot = xpath.evaluate(
+            lot = xpath.evaluate(
                 "//body/items/item/longitude",
                 it, XPathConstants.NODESET
             ) as NodeList?
-            var cntername = xpath.evaluate(
+            cntername = xpath.evaluate(
                 "//body/items/item/cnterNm",
                 it, XPathConstants.NODESET
             ) as NodeList?
-            var lnmadr = xpath.evaluate(
+            lnmadr = xpath.evaluate(
                 "//body/items/item/lnmadr",
                 it, XPathConstants.NODESET
             ) as NodeList?
-            var imbcltyIntrcn = xpath.evaluate(
+            imbcltyIntrcn = xpath.evaluate(
                 "//body/items/item/imbcltyIntrcn",
                 it, XPathConstants.NODESET
+            ) as NodeList?
+            call = xpath.evaluate(
+                    "//body/items/item/phoneNumber",
+                    it, XPathConstants.NODESET
+            ) as NodeList?
+            doctorN = xpath.evaluate(
+                    "//body/items/item/doctrCo",
+                    it, XPathConstants.NODESET
+            ) as NodeList?
+            nurseN = xpath.evaluate(
+                    "//body/items/item/nurseCo",
+                    it, XPathConstants.NODESET
             ) as NodeList?
 
             for(i in 0..lat?.length!!-1){
 
             mMap.addMarker(
-                MarkerOptions().position(LatLng(lat.item(i).textContent.toDouble(), lot?.item(i)?.textContent!!.toDouble()))
+                MarkerOptions().position(LatLng(lat!!.item(i).textContent.toDouble(), lot?.item(i)?.textContent!!.toDouble()))
                     .title(cntername!!.item(i).textContent)
+                        .zIndex(i.toFloat())
             )}
 
         })
@@ -95,6 +115,20 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         val marker = LatLng(35.241615, 128.695587)
         mMap.addMarker(MarkerOptions().position(marker).title("Marker LAB"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker,7f))
+        mMap.setOnMarkerClickListener(this)
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        var index = p0!!.zIndex.toInt()
+        var intent: Intent = Intent(context, DetailActivity::class.java)
+        intent.putExtra("cntername",cntername!!.item(index).textContent)
+        intent.putExtra("lnmadr",lnmadr!!.item(index).textContent)
+        intent.putExtra("imbcltyIntrcn",imbcltyIntrcn!!.item(index).textContent)
+        intent.putExtra("call",call!!.item(index).textContent)
+        intent.putExtra("doc",doctorN!!.item(index).textContent)
+        intent.putExtra("nur",nurseN!!.item(index).textContent)
+        startActivity(intent)
+        return true
     }
 
 }
